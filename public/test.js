e52d6676f0a33500ce998f2a39f2514bde4eae5d
sesankh <sesankh@gmail.com>
2022-06-04 17:38:03 +0530

    Second commit

--- public/test.js
+++ public/test.js
@@ -4,19 +4,31 @@ let latestStoriesEle = document.getElementById('latestStories');
 function createAndAppend(latestStory) {
     let {
         title,
-        link
+        link,
+        date
     } = latestStory;
 
     let container = document.createElement("div");
 
     let titleEle = document.createElement("a");
+   
     titleEle.href = link;
     titleEle.classList.add("active");
+    titleEle.classList.add("latest-stories-text");
     titleEle.target = '-blank';
     titleEle.textContent = title;
-    titleEle.classList.add("latest-stories-text");
     container.appendChild(titleEle);
+    
+    let dateEle = document.createElement("p");
+    dateEle.textContent = date;
+    console.log(date);
+    dateEle.classList.add("dateEle");
+    container.appendChild(dateEle);
+
+    
+    
     latestStoriesEle.appendChild(container);
+
     let titleBreakEle = document.createElement("br");
     latestStoriesEle.appendChild(titleBreakEle);
 
@@ -25,28 +37,34 @@ function createAndAppend(latestStory) {
 let latestStories = [
     {
     "title": "Amy Schneider’s Jeopardy! Streak Ends at 40 Consecutive Wins and $1.4 Million",
-     "link" : "https://time.com/6142934/amy-schneider-jeopardy-streak-ends/"
+     "link" : "https://time.com/6142934/amy-schneider-jeopardy-streak-ends/",
+      "date" : "JANUARY 26, 2022 * 11:43 PM EST"
     },
     {
     "title": "'Writing With Fire' Shines a Light on All-Women News Outlet",
-    "link": "https://time.com/6142628/writing-with-fire-india-documentary/"
+    "link": "https://time.com/6142628/writing-with-fire-india-documentary/",
+    "date" : "JANUARY 26, 2022 * 9:00 PM EST"
     },
     {
     "title": "Moderna Booster May Wane After 6 Months",
-    "link": "https://time.com/6142852/moderna-booster-wanes-omicron/"
+    "link": "https://time.com/6142852/moderna-booster-wanes-omicron/",
+    "date" : "JANUARY 26, 2022 * 5:51 PM EST"
     },
     {
     "title": "Pressure Mounts for Biden to Nominate a Black Woman to the Supreme",
     "link":
-    "https://time.com/6142743/joe-biden-supreme-court-nominee-black-woman-campaignpromise/"
+    "https://time.com/6142743/joe-biden-supreme-court-nominee-black-woman-campaignpromise/",
+    "date" : "JANUARY 26, 2022 * 3:51 PM EST"
     },
     {
     "title": "The James Webb Space Telescope Is in Position—And Now We Wait",
-    "link": "https://time.com/6142769/james-webb-space-telescope-reaches-l2/"
+    "link": "https://time.com/6142769/james-webb-space-telescope-reaches-l2/",
+    "date" : "JANUARY 26, 2022 * 3:46 PM EST"
     },
     {
     "title": "We Urgently Need a New National COVID-19 Response Plan",
-    "link": "https://time.com/6142718/we-need-new-national-covid-19-response-plan/"
+    "link": "https://time.com/6142718/we-need-new-national-covid-19-response-plan/",
+    "date" : "JANUARY 26, 2022 * 3:13 PM EST"
     }
     ]  
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 const express =require('express');
 const app = express();
 const path = require('path');
-// const staticPath = path.join(__dirname, '../public');
-// app.use(express.static(staticPath));
 
 const staticPath = path.join(__dirname,'../public');
 
 app.use(express.static(staticPath));
 
-app.get('/', (request, response) => {
-    response.sendFile(staticPath + 'test.html');
+
+app.get('/getTimeStories', (request, response) => {
+    response.sendFile(staticPath + '/test.html');
     
 });
 
 app.listen(9800, () => {
-    console.log("loading at port 9500")
+    console.log("loading at port 9800")
 });
